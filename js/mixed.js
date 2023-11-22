1df0cde688d73c77fa48ece0186f083c281173e8
SarunasT <sarunas.tarvydas@hotmail.com>
2023-11-22 10:43:50 +0100

    mixt file

---
+++ js/mixed.js
@@ -0,0 +1,18 @@
+"use strict";
+console.log("mixed.js file was loaded");
+
+const mixedArray = [
+  "banana",
+  1,
+  "giraffe",
+  "pizza",
+  true,
+  2,
+  false,
+  "unicorn",
+  7,
+  "laughter",
+  true,
+  "and",
+  3,
+];
