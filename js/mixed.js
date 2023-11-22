780cbe182bdac3cb6c656b2fff6ed2062faf17f6
SarunasT <sarunas.tarvydas@hotmail.com>
2023-11-22 11:07:46 +0100

    masivo funkcija

--- js/mixed.js
+++ js/mixed.js
@@ -16,3 +16,25 @@ const mixedArray = [
   "and",
   3,
 ];
+
+// sukurti funkcija kuri ima masyva kaip argument
+// a; gauna visu jo skaiciu suma
+// b; gauna visu jo skaiciu vidurky
+
+/**
+ *
+ * @param {(string | number | boolean)[]} arr
+ */
+function calculateFromArr(arr) {
+  let suma = 0;
+  // sukti cikla per masyva
+  arr.forEach((elementas) => {
+    // patikrinti ar tipas yra sk
+    if (typeof elementas === "number") {
+      // jei sk tai sudeti su kazkuo
+      suma += elementas;
+    }
+  });
+  return suma;
+}
+console.log(calculateFromArr(mixedArray));
